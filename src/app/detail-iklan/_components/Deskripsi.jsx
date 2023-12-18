f5637a23b0edd119746bf97720e8baa1fd47d2d0
misbahabroruddin <misbahabroruddin@gmail.com>
2023-12-18 09:55:34 +0700

    feat: add deskripsi component for detail iklan

---
+++ src/app/detail-iklan/_components/Deskripsi.jsx
@@ -0,0 +1,48 @@
+export default function Deskripsi() {
+  return (
+    <div className="flex flex-col gap-5 rounded-xl bg-white p-5">
+      <div className="flex grow justify-between border-b border-[#E5E5E5]">
+        <p className="mb-5 text-[10px] font-[500] text-[#AAAAAA] md:text-xs">
+          DESKRIPSI
+        </p>
+        <p className="mb-5 text-[10px] font-[500] text-[#AAAAAA] md:text-xs">
+          ID IKLAN : 98612486764
+        </p>
+      </div>
+      <p className="text-sm font-[500]">
+        Land Rover Range Rover 3.0 2013 AT Putih <br />
+        Harga Rp 615,000,000 (Nego di tempat sampai DEAL) <br />
+        <br />
+        Dijual : Land Rover Range Rover Sports HSE <br />
+        - Tahun : 2013 <br />
+        - Warna : Putih <br />
+        - Mesin : 2.000 CC, Bensin, AT <br />
+        - Kilometer : 54128 KM <br />
+        - Fitur : Shock Depan belakang Baru 1 Set 70 jutaan, Aki Baru, Oli Oli
+        Baru, Double Electric & Leather Seat, Audiosteer Control, LCD, Dual AC,
+        Sunroof, 2 Baris 5 Penumpang, Ban Serep <br />
+        - Surat : Lengkap, PLAT L Rungkut Surabaya, STNK, BPKB, Faktur Komplit,
+        Pajak 15-02-2023, Pajak Plat 15-02-2023 <br />
+        - Lokasi : Lenmarc Mall Surabaya <br />
+        - Harga : Rp. 615.000.000,- NEGO <br />
+        - Unit sudah lulus Inspeksi dan Bersertifikat <br />
+        - Dokumen lengkap (bpkb & faktur) ADA Semua, dijamin AMAN dari kasus
+        hukum <br />
+        - Mesin kering, tidak ada rembesan Oli sama sekali <br />
+        - Kondisi interior dan eksterior Bagus dan Terawat <br />
+        - Silakan di cek sepuasnya di tempat, <br />
+        <br />
+        Buka Setiap Hari (Minggu & Tanggal Merah TETAP BUKA) 09.00 - 21.00 WIB{" "}
+        <br />
+        Dapatkan fasilitas tambahan berupa : <br />
+        - Unit sdh lulus inspeksi & Bersertifikat <br />
+        - Garansi 7 Hari Uang Kembali <br />
+        - Garansi Mesin 30 hari <br />
+        - Voucher OLI Mesin dan Perawatan di SHOP & DRIVE <br />- Special HOME
+        TEST DRIVE (unit kami bawa kerumah anda utk test drive) <br />
+        *s&k berlaku <br />
+        Terimakasih <br />
+      </p>
+    </div>
+  )
+}
